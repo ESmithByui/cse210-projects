d87367d36d069ca287a795ee3c2d50c268e4149a
ESmithByui <smi20055@byui.edu>
2024-04-25 09:25:48 -0600

    Adjusted code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,11 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("What is your first name?");
-
         string first_name = Console.ReadLine();
 
         Console.WriteLine("What is your last name?");
-
         string last_name = Console.ReadLine();
 
         Console.WriteLine($"\nYour name is {last_name}, {first_name} {last_name}.");
