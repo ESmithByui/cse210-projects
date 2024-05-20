6bcb9e2816a1c84b65178c1c7508c5c0226592ca
ESmithByui <smi20055@byui.edu>
2024-05-20 13:28:43 -0600

    Finished prepare 2

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction test1 = new Fraction();
+        Console.WriteLine(test1.GetFractionString());
+        Console.WriteLine(test1.GetDecimalValue());
+
+        Fraction test2 = new Fraction(5);
+        Console.WriteLine(test2.GetFractionString());
+        Console.WriteLine(test2.GetDecimalValue());
+
+        Fraction test3 = new Fraction(3,4);
+        Console.WriteLine(test3.GetFractionString());
+        Console.WriteLine(test3.GetDecimalValue());
+
+        test3.SetTop(1);
+        test3.SetBottom(3);
+        Console.WriteLine(test3.GetFractionString());
+        Console.WriteLine(test3.GetDecimalValue());
+
     }
 }
