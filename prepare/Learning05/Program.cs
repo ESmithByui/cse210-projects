b110fdb6afcc371456c901c68c202869754e6230
ESmithByui <smi20055@byui.edu>
2024-06-17 15:22:30 -0600

    shapes prep

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.ObjectModel;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("red", 5),
+            new Rectangle("blue", 3, 4.5),
+            new Circle("green", 10)
+        };
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine($"{shape.GetColor()} | {shape.GetArea()}");
+        }
     }
 }
